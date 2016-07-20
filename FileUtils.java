85185a5388e32437d187beb554df3828c16a4876
markozeman <mz4730@student.uni-lj.si>
2016-07-20 19:22:56 +0000

    5x GUI

---
+++ FileUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dn03;
+
+import java.io.File;
+
+/**
+ *
+ * @author student
+ */
+class FileUtils {
+
+    static void writeStringToFile(File file, String hello_world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
